b535a1229b3c8c156d27dcc2d64728b60d247430
Allen <lygttpod@163.com>
2015-12-28 22:04:41 +0800

    修复每次切换tab时候fragment重绘UI的问题

--- app/src/main/java/com/allen/tabhostdemo/fragment/HomeFragment.java
+++ app/src/main/java/com/allen/tabhostdemo/fragment/HomeFragment.java
@@ -3,6 +3,7 @@ package com.allen.tabhostdemo.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +14,21 @@ import com.allen.tabhostdemo.R;
  * Created by Allen on 15/12/27.
  */
 public class HomeFragment extends Fragment {
+    private View rootView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_home,container,false);
-        return view;
+
+        if (rootView == null) {
+            rootView = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_home, container, false);
+        }
+        // 缓存的rootView需要判断是否已经被加过parent，如果有parent需要从parent删除，
+        // 要不然会发生这个rootview已经有parent的错误。
+        ViewGroup parent = ((ViewGroup) rootView.getParent());
+        if (parent != null) {
+            parent.removeView(rootView);
+        }
+        return rootView;
     }
 }
--- app/src/main/java/com/allen/tabhostdemo/fragment/MineFragment.java
+++ app/src/main/java/com/allen/tabhostdemo/fragment/MineFragment.java
@@ -3,6 +3,7 @@ package com.allen.tabhostdemo.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +17,11 @@ public class MineFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d("allen", "MineFragment------onCreateView");
+
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_mine,container,false);
+        Log.d("allen","MineFragment------rootView==null");
+
         return view;
     }
 }
